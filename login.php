665070f6feb877550dc05a9394b8ee51945c781e
jenofiya2005 <jeno30justin@gmail.com>
2026-08-08 02:22:11 +0530

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 
 header('Content-Type: application/json');
 require __DIR__ . '/db_mysql.php';
-require __DIR__ . '/../vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     echo json_encode(["success" => false, "message" => "Invalid request method"]);
